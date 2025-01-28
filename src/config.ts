2226c47b76c2915759ad578bc3f89b9fee7d95f4
yylbfyl <yylbfyl@163.com>
2025-01-28 09:36:45 -0500

    update

--- src/config.ts
+++ src/config.ts
@@ -70,7 +70,7 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/yylbfyl',
     },
   ],
 }
